881c42bdab28920c15a37a9f2ce852a6af529dd9
bbbence <benceborbas88@gmail.com>
2020-11-24 03:11:36 +0100

    Modal exercise.

---
+++ script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// Get the modal
+let modal = document.getElementById("theModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("modalBtn");
+
+let btn2 = document.getElementById("okBtn")
+
+let btn3 = document.getElementById("cancelBtn")
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+btn2.onclick = function() {
+    modal.style.display = "none";
+}
+
+btn3.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
